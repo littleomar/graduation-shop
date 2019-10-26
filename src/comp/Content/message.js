0b7570a5f70269ff41a6ec88c6b3523008a3b283
gongsc <gongshichao@58qf.com>
2019-10-26 10:32:38 +0800

    增加菱形

--- src/comp/Content/message.js
+++ src/comp/Content/message.js
@@ -1,12 +1,38 @@
 import React, {Fragment} from "react";
 import { Carousel } from "antd";
-import Content1 from './jy1.js';
 import MessageStyles from './message.module.css'
 import banner1 from '../../statics/banner1.png'
 import banner2 from '../../statics/banner2.png'
 import AppStyles from "../../App.module.css";
+import headImg1 from '../../statics/zd1.png';
+import decImg1 from '../../statics/1.png';
 
 class Message extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        mockData: [
+          {
+            title: '专业的指导老师',
+            content: '每一名指导老师都经过专业培训，德智体美劳全面教育。 每一名指导老师都有着懂宝宝，关心宝宝的心。 让每一位孩子都可以得到最细致，最温暖，最贴心的爱护。 保证让每一位宝宝都可以健健康康，快快乐乐的成长。',
+            headImg: headImg1,
+            decImg: decImg1,
+          },
+          {
+            title: '专业的指导老师',
+            content: '每一名指导老师都经过专业培训，德智体美劳全面教育。 每一名指导老师都有着懂宝宝，关心宝宝的心。 让每一位孩子都可以得到最细致，最温暖，最贴心的爱护。 保证让每一位宝宝都可以健健康康，快快乐乐的成长。',
+            headImg: headImg1,
+            decImg: decImg1,
+          },
+          {
+            title: '专业的指导老师',
+            content: '每一名指导老师都经过专业培训，德智体美劳全面教育。 每一名指导老师都有着懂宝宝，关心宝宝的心。 让每一位孩子都可以得到最细致，最温暖，最贴心的爱护。 保证让每一位宝宝都可以健健康康，快快乐乐的成长。',
+            headImg: headImg1,
+            decImg: decImg1,
+          },
+        ]
+    }
+  }
 
   render() {
     return (
@@ -23,11 +49,37 @@ class Message extends React.Component {
           我们有最优秀，最全面的教育机构
         </div>
         <div className={MessageStyles.babyWrapper}>
-          <div className={MessageStyles.babyItem}>
-            <Content1 />
+          {
+            this.state.mockData.map(item =>
+              <div className={MessageStyles.babyItem}>
+                <div>
+                  <img className={MessageStyles.headImg} src={item.headImg} alt=""/>
+                </div>
+                <div className={MessageStyles.titleWrapper}>
+                  <img src={item.decImg} alt=""/>
+                  <div className={MessageStyles.titleTxt}>{item.title}</div>
+                </div>
+                <div className = {MessageStyles.contentTxt}>{item.content}</div>
+              </div>
+            )
+          }
+        </div>
+        <div className={MessageStyles.babyTitle}>
+          我们有最优秀，最全面的教育机构
+        </div>
+        <div className = {[MessageStyles.babyWrapper]}>
+          <div className = {`${MessageStyles.babyItem} ${MessageStyles.shapeItem}`}>
+            <div className={MessageStyles.diamond}>aaaa</div>
+            <div className = {MessageStyles.shapeContent}>
+              <p>b</p>
+              <p>b</p>
+              <p>b</p>
+              <p>b</p>
+              <p>b</p>
+              <p>b</p>
+              <p>b</p>
+            </div>
           </div>
-          <div className={MessageStyles.babyItem}>2</div>
-          <div className={MessageStyles.babyItem}>3</div>
         </div>
       </Fragment>
     )
