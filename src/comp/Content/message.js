232917c4a9f89220ca8b648b624d4c8ec754f2d9
zyp <2659303851@qq.com>
2019-10-28 10:05:35 +0800

    修改

--- src/comp/Content/message.js
+++ src/comp/Content/message.js
@@ -5,7 +5,14 @@ import banner1 from '../../statics/banner1.png'
 import banner2 from '../../statics/banner2.png'
 import AppStyles from "../../App.module.css";
 import headImg1 from '../../statics/zd1.png';
+import headImg2 from '../../statics/xx1.png';
+import headImg3 from '../../statics/hj1.png';
 import decImg1 from '../../statics/1.png';
+import decImg2 from '../../statics/-2.png';
+import decImg3 from '../../statics/3.png';
+import xx1 from '../../statics/003.png';
+import xx2 from '../../statics/005.png';
+import xx3 from '../../statics/006.png';
 
 class Message extends React.Component {
   constructor() {
@@ -19,16 +26,16 @@ class Message extends React.Component {
             decImg: decImg1,
           },
           {
-            title: '专业的指导老师',
-            content: '每一名指导老师都经过专业培训，德智体美劳全面教育。 每一名指导老师都有着懂宝宝，关心宝宝的心。 让每一位孩子都可以得到最细致，最温暖，最贴心的爱护。 保证让每一位宝宝都可以健健康康，快快乐乐的成长。',
-            headImg: headImg1,
-            decImg: decImg1,
+            title: '快乐轻松的学习',
+            content: '根据Play Value为孩子精心设计的教具，激发孩子自主探索和学习的兴趣。我们将通过与父母玩耍的方式，让孩子自己主动的探索学习，激发孩子的自信。通过舞蹈乐器的演奏，让孩子在音乐中加强对身体的控制和对动作的表达。',
+            headImg: headImg2,
+            decImg: decImg3,
           },
           {
-            title: '专业的指导老师',
-            content: '每一名指导老师都经过专业培训，德智体美劳全面教育。 每一名指导老师都有着懂宝宝，关心宝宝的心。 让每一位孩子都可以得到最细致，最温暖，最贴心的爱护。 保证让每一位宝宝都可以健健康康，快快乐乐的成长。',
-            headImg: headImg1,
-            decImg: decImg1,
+            title: ' 温馨宽阔的空间',
+            content: '配合金宝贝课程特别研发，我们为孩子打造森林、湖泊、火山、海洋等不同主题的教室设计，从孩子的视角出发，为孩子开启无限想象的空间。多元宽敞的空间，和经验丰富的指导师，让父母与孩子共享高质量的亲子时光。',
+            headImg: headImg3,
+            decImg: decImg2,
           },
         ]
     }
@@ -65,19 +72,34 @@ class Message extends React.Component {
           }
         </div>
         <div className={MessageStyles.babyTitle}>
-          我们有最优秀，最全面的教育机构
+          我们有最贴心，细致的教育系统
         </div>
         <div className = {[MessageStyles.babyWrapper]}>
           <div className = {`${MessageStyles.babyItem} ${MessageStyles.shapeItem}`}>
-            <div className={MessageStyles.diamond}>aaaa</div>
+            <div className={MessageStyles.diamond}>
+              <img src={xx1} alt="" />
+            </div>
             <div className = {MessageStyles.shapeContent}>
-              <p>b</p>
-              <p>b</p>
-              <p>b</p>
-              <p>b</p>
-              <p>b</p>
-              <p>b</p>
-              <p>b</p>
+              <p>从0岁到到三岁。
+                课程提供适龄游戏，帮助家长发现宝宝的兴趣，让宝宝在过程中不断探索自己的能力。</p>
+            </div>
+          </div>
+          <div className = {`${MessageStyles.babyItem} ${MessageStyles.shapeItem}`}>
+            <div className={MessageStyles.diamond}>
+              <img src={xx2} alt="" />
+            </div>
+            <div className = {MessageStyles.shapeContent}>
+              <p>从6个月到五岁
+                20种音乐风格、乐器的使用，让家长掌握与孩子共同探索音乐的方式。</p>
+            </div>
+          </div>
+          <div className = {`${MessageStyles.babyItem} ${MessageStyles.shapeItem}`}>
+            <div className={MessageStyles.diamond}>
+              <img src={xx3} alt="" />
+            </div>
+            <div className = {MessageStyles.shapeContent}>
+              <p>从18个月到5岁
+                别出心裁的艺术工具，丰富孩子对自我和环境的表达和探索提高孩子的表达力、专注力、审美能力。</p>
             </div>
           </div>
         </div>
