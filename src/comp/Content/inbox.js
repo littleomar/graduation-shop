9c41bce8b7a1344cb0f11a676c61b4a4c2073402
zyp <2659303851@qq.com>
2019-11-17 15:35:26 +0800

    修改11/17 class

--- src/comp/Content/inbox.js
+++ src/comp/Content/inbox.js
@@ -1,6 +1,8 @@
 import React from "react";
 import style from './inbox.module.css';
 import axios from "axios";
+import MessageStyles from "./message.module.css";
+
 
 
 class InBox extends React.Component {
@@ -24,6 +26,35 @@ class InBox extends React.Component {
               <img src={item.banner}/>
             ))
           }
+        </div>
+        <div className={style.headtitle}>
+          我们的教具<br/>
+          OUR TEACHING AIDS
+        </div>
+        <div className={style.aids}>
+
+        </div>
+        <div className={style.footall}>
+          <div className={style.footcontent}>
+              <div className={style.foottitle}>
+                育乐教室
+                <br/>
+                Recreational classroom
+              </div>
+            <div className={style.footWrapper}>
+              {
+                this.state.classContent.map(item => (
+                  <div className={style.footimg}>
+                    <div>
+                      <img src={this.state.classContent.aImg}/>
+                      <img src={this.state.classContent.bImg}/>
+                      <img src={this.state.classContent.cImg}/>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
         </div>
       </div>
     );
